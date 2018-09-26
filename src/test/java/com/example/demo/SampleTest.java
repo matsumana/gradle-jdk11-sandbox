7263d5d160f74f3ff317101ac0db1c640b6e26a1
Manabu Matsuzaki <matsumana@gmail.com>
2018-09-26 23:30:19 +0900

    improve class name

--- src/test/java/com/example/demo/SampleTest.java
+++ src/test/java/com/example/demo/SampleTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 public class SampleTest {
 
-    public static class InnerClass {
+    public static class NestClass {
         private String field1;
 
         public String getField1() {
