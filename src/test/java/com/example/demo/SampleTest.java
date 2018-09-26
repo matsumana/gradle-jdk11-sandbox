78eeb8ef3311d1c6ad31fafa7c016a5c26171bde
Manabu Matsuzaki <matsumana@gmail.com>
2018-09-26 21:05:47 +0900

    first commit

---
+++ src/test/java/com/example/demo/SampleTest.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+
+    public static class InnerClass {
+        private String field1;
+
+        public String getField1() {
+            return field1;
+        }
+
+        public void setField1(String field1) {
+            this.field1 = field1;
+        }
+    }
+
+    @Test
+    public void test() throws Exception {
+        assertTrue(true);
+    }
+}
